c031008d5c646e6fe1981b026bdd796b51173dec
suketa <suketa.sharma@meradata.com>
2012-12-27 15:42:55 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectMgt::Application.config.secret_token = 'e4e79bcb2099c3ecdb6e4f61813a29cec990266c1a7719b6431d480efb6873d38b60e142f45b45d83b1d1e920f1264accdcde2f0984cb7b7e58b94eadd1f730d'
